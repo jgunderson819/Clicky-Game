2f9eb31cc3f84ee6c5482b911dcab4461802fa1f
Jeff Gunderson <jgunderson819@gmail.com>
2020-04-26 18:21:11 -0500

    Updates

---
+++ precache-manifest.24f6d5e7f6b6794fb8ec41c76311af45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c98088bc0b8e113202f34d5f3613764",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "83c325d2ad9536f9aee1",
+    "url": "/Clicky-Game/static/css/main.ef5f330b.chunk.css"
+  },
+  {
+    "revision": "6317c9b25b6891613dd1",
+    "url": "/Clicky-Game/static/js/2.242e5220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.242e5220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c325d2ad9536f9aee1",
+    "url": "/Clicky-Game/static/js/main.80626328.chunk.js"
+  },
+  {
+    "revision": "df62fdd814dd0848a866",
+    "url": "/Clicky-Game/static/js/runtime-main.f9b63343.js"
+  }
+]);
